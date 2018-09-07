98a76a806c618b85545e9b34f372c5e562adab60
Evandro Pavei <epavei@coinbr.net>
2018-09-07 13:47:03 -0300

    handling errors

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 Folder dist with build files must be inside this server folder.
+
+Generate build
+GOOS=linux GOARCH=amd64 go build 
+
+scp -i ~/.ssh/pv-alert-03-09-18.pem ./go-frontend-server ubuntu@ec2-18-205-247-206.compute-1.amazonaws.com:/home/ubuntu/market/
+
+```
+ [Unit]
+  Description=Go Server
+
+  [Service]
+  ExecStart=/home/ubuntu/market/go-frontend-server
+  User=root
+  Group=root
+  Restart=always
+
+  [Install]
+  WantedBy=multi-user.target
+  ```
--- main.go
+++ main.go
@@ -7,12 +7,16 @@ import (
 )
 
 func main() {
-	dir, _ := os.Getwd()
-	fs := http.FileServer(http.Dir(dir + "/dist"))
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	distPath := dir + "/dist"
+	fs := http.FileServer(http.Dir(distPath))
 	http.Handle("/", fs)
 	http.HandleFunc("/*", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "index.html")
 	})
-	log.Println("Serving " + dir)
-	http.ListenAndServe(":80", nil)
+	log.Println("Serving " + distPath)
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
